e4c63f5b1210c50ba6a96e831fdcb99e58ca36cf
gong7788 <44057714+gong7788@users.noreply.github.com>
2018-11-13 19:14:52 +0000

    Update MoneyTest.java

--- AuctionHouse/src/auctionhouse/MoneyTest.java
+++ AuctionHouse/src/auctionhouse/MoneyTest.java
@@ -5,6 +5,7 @@ package auctionhouse;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 /**
