a6e92c6dd8bf42cf05f8f61ae2519fadf5ce7c7b
robbieranson <44927960+robbieranson@users.noreply.github.com>
2018-11-18 15:37:56 +0000

    Update Seller.java

--- AuctionHouse/src/auctionhouse/Seller.java
+++ AuctionHouse/src/auctionhouse/Seller.java
@@ -3,12 +3,13 @@ package auctionhouse;
 public class Seller {
 	private String name;
 	private String address;
-    private String bankAccount;
+    	private String bankAccount;
 	
 	public Seller(String name, String address, String bankAcount) {
 		name = this.name;
 		address = this.address;
 		bankAcount = this.bankAccount;
 	}
+	
 
 }
