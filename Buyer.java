4febf88748d0efa905c78df726684ca69d5ce1f1
robbieranson <44927960+robbieranson@users.noreply.github.com>
2018-11-18 15:51:59 +0000

    Update Buyer.java

--- Buyer.java
+++ Buyer.java
@@ -3,14 +3,14 @@ package auctionhouse;
 public class Buyer {
 	private String name;
 	private String address;
-    private String bankAccount;
-    private String bankAuthCode;
+    	private String bankAccount;
+    	private String bankAuthCode;
 	
 	public Buyer(String name, String address, String bankAcount, String bankAuth) {
-		name = this.name;
-		address = this.address;
-		bankAcount = this.bankAccount;
-		bankAuth = bankAuthCode;
+		this.name = name;
+		this.address = address;
+		this.bankAcount = bankAccount;
+		this.bankAuthCode = bankAuth;
 	}
 	
 	
